ef19499ca34b86f0ca994736683830f8c6241a64
tranzi90 <tranzi90@gmail.com>
2020-09-29 13:39:36 +0300

    fix start script

--- app.js
+++ app.js
@@ -11,13 +11,19 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/link', require('./routes/link'))
 app.use('/t', require('./routes/redirect'))
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+// if (process.env.NODE_ENV === 'production') {
+//     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+//
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000
 
