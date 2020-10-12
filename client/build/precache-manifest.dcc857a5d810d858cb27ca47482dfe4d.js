a8c85b41ef1ece66e898ae7b5acdc8d89f8b344b
tranzi90 <tranzi90@gmail.com>
2020-10-12 13:47:52 +0300

    test build

---
+++ client/build/precache-manifest.dcc857a5d810d858cb27ca47482dfe4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2621a3a417099d7ab7ed224bdbfb188f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c079577356542b69f165",
+    "url": "/static/css/2.1fce433c.chunk.css"
+  },
+  {
+    "revision": "8bcb44b76419cd1f9cfb",
+    "url": "/static/css/main.cf536487.chunk.css"
+  },
+  {
+    "revision": "c079577356542b69f165",
+    "url": "/static/js/2.0db2697c.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/static/js/2.0db2697c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bcb44b76419cd1f9cfb",
+    "url": "/static/js/main.12cae8a2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
