8beaa1cf7edfd818f87aa208d366ed11c854f243
denis_levchenko <tranzi90@gmail.com>
2021-07-28 22:00:19 +0300

    title change

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import Navbar from "./components/Navbar"
 import Loader from "./components/Loader"
 import 'materialize-css'
 
-
 function App() {
     const {token, login, logout, userId, ready} = useAuth()
     const isAuthenticated = !!token
